aef0294ac5201f369100742cb0df200637fa2087
Prashant <prashant@gyeser.com>
2014-07-17 19:22:55 +0530

    Added test and a fucntion to see if a positive operator is available in a tweet. Still need to tie all this together

--- lib/keyword_checker.rb
+++ lib/keyword_checker.rb
@@ -6,7 +6,13 @@ class KeywordChecker
   # The keywords to check are listed above, add all the keywords here.
   def self.keyword_in_tweet?(tweet_text)
     keywords_to_find_meanings = ['salsa', 'bachata', 'merengue', 'dance', 'dances', 'dancing', 'danced','dancer', 'latin', 'cuban', 'groove', 'boogaloo', 'party', 'mambo', 'music','move', 'spanish', 'waltz']
-    keywords_to_find_meanings.any? { |w| tweet_text =~ /#{w}/ }
+    # Refer http://stackoverflow.com/a/5482903
+    keywords_to_find_meanings.any? { |w| tweet_text.downcase =~ /#{w}/ }
+  end
+
+  def self.positive_operator_in_tweet?(tweet_text)
+    positive_operators = ['like','love','want','learn','learned','learning','wanna','teach']
+    positive_operators.any? { |w| tweet_text.downcase =~ /#{w}/ }
   end
 end
 
--- spec/lib/keyword_checker_spec.rb
+++ spec/lib/keyword_checker_spec.rb
@@ -1,7 +1,29 @@
 require '../spec/spec_helper'
 
 RSpec.describe KeywordChecker do
+
   it "should do an exact match for a keyword" do
     expect(KeywordChecker.keyword_in_tweet?("salsa")).to eq(true)
   end
+
+  it "should ignore case when checking a keyword" do
+    expect(KeywordChecker.keyword_in_tweet?("Salsa")).to eq(true)
+  end
+
+  it "should not match a substring keyword" do
+    expect(KeywordChecker.keyword_in_tweet?("salsathis is")).to eq(true)
+  end
+
+  it "should do an exact match for a positive operator" do
+    expect(KeywordChecker.positive_operator_in_tweet?("like")).to eq(true)
+  end
+
+  it "should ignore case when checking a positive operator" do
+    expect(KeywordChecker.positive_operator_in_tweet?("Like")).to eq(true)
+  end
+
+  it "should not match a substring for a positive operator" do
+    expect(KeywordChecker.positive_operator_in_tweet?("likethis is")).to eq(true)
+  end
+
 end
